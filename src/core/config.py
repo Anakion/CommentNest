afc4fbb23a370e332c8ecba4db11ce1c6e3d060e
Anakion <andreygolosov786@gmail.com>
2025-09-23 12:07:00 +0300

    chore(config): add project configuration

---
+++ src/core/config.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+
+class Settings(BaseSettings):
+    GOOGLE_TOKEN_ID: str = "postgres"
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_DB: str = "postgres"
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    SECRET_KEY: str = "4d90daed8f8c54d87c7e003523a5036a"
+    ALGORITHM: str = "HS256"
+    ECHO: bool = True
+
+    @property
+    def database_url(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_USER}"
+            f":{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}"
+            f":{self.POSTGRES_PORT}"
+            f"/{self.POSTGRES_DB}"
+        )
+
+    model_config = SettingsConfigDict(env_file=BASE_DIR / ".dev.env")
+
+
+settings = Settings()
