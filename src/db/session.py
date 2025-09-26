4613133a84b9c9ea8db5aaf54242dafb8dd7a256
Anakion <andreygolosov786@gmail.com>
2025-09-26 10:06:47 +0300

    feat get_db func for GeneratorSession

--- src/db/session.py
+++ src/db/session.py
@@ -1,3 +1,5 @@
+from typing import AsyncGenerator
+
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
 from src.db.base import engine
@@ -7,3 +9,8 @@ AsyncSessionLocal = async_sessionmaker(
     class_=AsyncSession,
     expire_on_commit=False,
 )
+
+
+async def get_db() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionLocal() as session:
+        yield session
