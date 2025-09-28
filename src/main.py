7bfcacec0010f951e09b4fc664a5192674ef2ee1
Anakion <andreygolosov786@gmail.com>
2025-09-28 17:13:31 +0300

    fix: Path static

--- src/main.py
+++ src/main.py
@@ -3,21 +3,20 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
 from src.api.v1.routers import comments, ws
-from src.core.websocket_manager import manager
 
 # Получаем абсолютный путь к корню проекта
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
 # Переходим на уровень выше (из src в корень проекта)
 BASE_DIR = os.path.dirname(PROJECT_ROOT)
 STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 app = FastAPI()
 
-# Подключаем роутеры API
 app.include_router(comments.router, prefix="/api/v1")
 app.include_router(ws.router, prefix="/api/v1")
 
-# Обслуживание статических файлов
+
 app.mount("/", StaticFiles(directory=STATIC_DIR, html=True), name="static")
 
 
