a61bbff0cb54b797b0ea094da0d04a5f25592ff0
Anakion <andreygolosov786@gmail.com>
2025-09-28 17:09:56 +0300

    fix: Changes in the environment

--- src/core/redis_core.py
+++ src/core/redis_core.py
@@ -1,5 +1,12 @@
 from redis.asyncio import Redis
+
+from src.core.config import settings
 from src.utils.redis_client import CacheComments
 
-redis_client = Redis(host="localhost", port=6379, db=0, decode_responses=True)
+redis_client = Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB,
+    decode_responses=True,
+)
 cache_comments = CacheComments(redis_cache=redis_client)
